ed34c0a9dc0a20de3731395452c13486e4e6b8a5
Marnee Dearman <marnee.dearman@gmail.com>
2019-02-28 00:30:13 -0700

    Issue #7: Added comments with reference sto ax25 odule

--- src/faprs.core/AX25.fs
+++ src/faprs.core/AX25.fs
@@ -1,4 +1,4 @@
-namespace faprs.domain
+namespace faprs.core
 
 //TODO build this out to make it possible to send directly to a TNC instead of using Direwolf kissutil
 
@@ -6,6 +6,14 @@ namespace faprs.domain
 
 https://en.wikipedia.org/wiki/AX.25
 https://tapr.org/pub_ax25.html
+
+AX.25.2.2.pdf
+3. Frame Structure
+Link layer packet radio transmissions are sent in small blocks of data, called frames.
+
+Flag        Address         Control     PID     Info        FCS     Flag
+01111110    112/224 Bits    8/16 Bits   8 Bits  N*8 Bits 1  6 Bits  01111110
+0x7E                                                                0x7E
 
 APR1.01 
 3 APRS AND AX.25
@@ -15,13 +23,20 @@ AX.25 UI-FRAME FORMAT
 
 *)
 module AX25 =
+    open System
 
     type Packet =
         {
-            Flag : byte[]
+            Flag : int8
             Address : byte[]
             Control : byte[]
             ProtocolIdentifier : byte[]
             Information: byte[]
             FrameCheckSequence: byte[]
         }
+        member this.ToByteArray() =
+            Array.empty
+            |> Array.append (BitConverter.GetBytes(0x7E))
+            |> Array.append this.Address
+            |> Array.append this.Control
+
