2d6e018bccf73b3492f5b361a94b4eab8649b602
Marnee Dearman <marnee.dearman@gmail.com>
2019-03-16 09:19:36 -0700

    renamed APRSDataExtension to APRSData because technically the position report is APRSData without a data extension

--- src/faprs.core/APRSData.fs
+++ src/faprs.core/APRSData.fs
@@ -6,7 +6,7 @@ namespace faprs.core
 //http://www.aprs.org/aprs11/SSIDs.txt
 
 [<AutoOpen>]
-module APRSDataExtensions = 
+module APRSData = 
 
     type PositionReportComment = private PositionReportComment of string
     module PositionReportComment =
@@ -48,6 +48,19 @@ module APRSDataExtensions =
     let hemisphereToString degrees hemisphereChar =
         sprintf "%.2f%c" degrees hemisphereChar
 
+    //TODO constrain the size of the Degrees field
+    (* 
+    APRS101.pdf Chapter 6: Time and Position Formats
+    Latitude is expressed as a fixed 8-character field, in degrees and decimal
+    minutes (to two decimal places), followed by the letter N for north or S for
+    south.
+
+    For example:
+    07201.75W is 72 degrees 1 minute 45 seconds west.
+    In generic format examples, the longitude is shown as the 9-character string
+    dddmm.hhW (i.e. degrees, minutes and hundredths of a minute west).
+
+    *)
     type Latitude =
         {
             Degrees : float
@@ -66,21 +79,30 @@ module APRSDataExtensions =
             hemisphereToString this.Degrees (this.Hemisphere.ToHemisphereChar())
             //sprintf "%.2f%c" this.Degrees (this.Hemisphere.ToHemisphereChar())
 
-//TODO constrain the size of the Degrees field
-// Latitude is expressed as a fixed 8-character field, in degrees and decimal
-// minutes (to two decimal places), followed by the letter N for north or S for
-// south.
-// Longitude is expressed as a fixed 9-character field, in degrees and decimal
-// minutes (to two decimal places), followed by the letter E for east or W for
-// west.
+    (* 
+    APRS101.pdf Chapter 6: Time and Position Formats
+    Longitude is expressed as a fixed 9-character field, in degrees and decimal
+    minutes (to two decimal places), followed by the letter E for east or W for
+    west.
 
+    For example:
+    07201.75W is 72 degrees 1 minute 45 seconds west.
+    In generic format examples, the longitude is shown as the 9-character string
+    dddmm.hhW (i.e. degrees, minutes and hundredths of a minute west).
+    *)
     type Position =
         {
             Latitude : Latitude 
             Longitude : Longitude
         }
 
-//TODO support more position report types -- data extensions
+    //TODO support more position report types -- data extensions
+    (*
+    APRS101.pdf Chapter 6: Time and Position Formats
+    Must end in a Symbol Code
+    Position coordinates are a combination of latitude and longitude, separated
+    by a display Symbol Table Identifier, and followed by a Symbol Code. 
+    *)
     type PositionReportWithoutTimeStamp =
         {
             Position : Position
@@ -88,26 +110,25 @@ module APRSDataExtensions =
             Comment : PositionReportComment
         }
         override this.ToString() =
-            //Must end in a Symbol Code
-            //Position coordinates are a combination of latitude and longitude, separated
-            //by a display Symbol Table Identifier, and followed by a Symbol Code. 
             sprintf "=%s/%s%c%s" (this.Position.Latitude.ToString()) (this.Position.Longitude.ToString()) (this.Symbol.ToChar()) (PositionReportComment.value this.Comment)
     
     type UnformattedMessage = private UnformattedMessage of string
     module UnformattedMessage =
-        let create (s:string) =
-            match (s.Trim()) with
-            | s when s.Length <= 255 -> UnformattedMessage s //AX.25 field is 256 chars but the message has to accomodate the { for user defined messages
-            | _ -> UnformattedMessage (s.Substring(0, 255)) //TODO or throw an exception?
-        let value (UnformattedMessage s) = s
+        let create (m:string) =
+            match (m.Trim()) with
+            | m when m.Length <= 255 -> UnformattedMessage m //AX.25 field is 256 chars but the message has to accomodate the { for user defined messages
+            | _ -> UnformattedMessage (m.Substring(0, 255)) //TODO or throw an exception?
+        let value (UnformattedMessage m) = sprintf ":%s" m
 
     type Message =
         | Unformatted                       of UnformattedMessage
         | PositionReportWithoutTimeStamp    of PositionReportWithoutTimeStamp
+        | ParticipantStatusReport           of Participant.ParitipantStatusReport
         override this.ToString() =
             match this with 
-            | Unformatted m     -> sprintf ":%s" (UnformattedMessage.value m) // (:) is the aprs data type ID for message
-            | PositionReportWithoutTimeStamp p  -> p.ToString()
+            | Unformatted m                     -> UnformattedMessage.value m // (:) is the aprs data type ID for message
+            | PositionReportWithoutTimeStamp r  -> r.ToString()
+            | ParticipantStatusReport r         -> r.ToString()
 
     type SSID =
         | PrimaryStation 
